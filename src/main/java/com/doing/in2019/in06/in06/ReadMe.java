f06d57febeb877fa40f832b48cb1afbba7c855d6
Clyde <lkwowfs@163.com>
2019-06-11 11:54:15 +0800

    在公司提交一下，最近学的都写在纸上了，晚上整理到代码上

---
+++ src/main/java/com/doing/in2019/in06/in06/ReadMe.java
@@ -0,0 +1,12 @@
+package com.doing.in2019.in06.in06;
+
+public class ReadMe {
+
+	/**
+	 * 搭建SpringBoot项目 groupId和artifactId是maven管理项目包时用作区分的字段，就像是地图上的坐标。包名根目录=groupId+artifactId。
+	 *
+	 *		groupId：groupId分为几个字段，例如com.piggy，前面的com叫【域】，后面的是你自己起的域名。
+	 *		artifactId：artifactId一般是项目名或者模块名。
+	 *
+	 */
+}
