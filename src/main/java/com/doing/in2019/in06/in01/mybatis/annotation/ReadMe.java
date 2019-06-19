fc4c23511ae708dcff4fee7562aa28f2d5671d35
Clyde <lkwowfs@163.com>
2019-06-19 15:43:23 +0800

    不信了，非得把这个奇怪解决

--- src/main/java/com/doing/in2019/in06/in01/mybatis/annotation/ReadMe.java
+++
@@ -1,8 +0,0 @@
-package com.doing.in2019.in06.in01.Mybatis.annotation;
-
-public class ReadMe {
-
-	/**
-	 * Mybatis（注解使用方式）
-	 */
-}
--- src/main/java/com/doing/in2019/in06/in01/mybatis/xml/ReadMe.java
+++
@@ -1,8 +0,0 @@
-package com.doing.in2019.in06.in01.Mybatis.xml;
-
-public class ReadMe {
-
-	/**
-	 * Mybatis（xml使用方式）
-	 */
-}
