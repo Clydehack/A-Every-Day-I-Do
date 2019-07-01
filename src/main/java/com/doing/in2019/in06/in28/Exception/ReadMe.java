44df2b327fc28108419992f3257fc4b4f9af4926
Clyde <lkwowfs@163.com>
2019-07-01 08:43:54 +0800

    提交一下

---
+++ src/main/java/com/doing/in2019/in06/in28/Exception/ReadMe.java
@@ -0,0 +1,19 @@
+package com.doing.in2019.in06.in28.Exception;
+
+public class ReadMe {
+
+	/** Excetpion -- 异常 */
+	
+	/** 首先，测试一下在正常代码里直接抛出会发生什么情况 (发现没，这个抛出没有要求捕获处理) */
+	public static void main(String[] args) {
+		throw new RuntimeException();
+	}
+	
+	
+	/**
+	 * Java可分为三种异常
+	 * 
+	 * 
+	 * 
+	 */
+}
