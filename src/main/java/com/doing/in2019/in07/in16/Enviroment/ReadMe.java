aca6990f43d681b3e72aff22e345cc2dbb481c3d
Clyde <lkwowfs@163.com>
2019-07-17 17:39:24 +0800

    提交一下这两天随笔

---
+++ src/main/java/com/doing/in2019/in07/in16/Enviroment/ReadMe.java
@@ -0,0 +1,15 @@
+package com.doing.in2019.in07.in16.Enviroment;
+
+public class ReadMe {
+
+	/**
+	 * 工作环境：
+	 * 
+	 * 		ppt装机教程
+	 * 		下一步通过脚本自动化安装配置！
+	 * 
+	 * 		redis、mongodb、mysql、HDFS、HBase
+	 * 		FastDB、FTP
+	 * 		ES、Kafka、RabbitMQ、Flume、Jenkins
+	 */
+}
