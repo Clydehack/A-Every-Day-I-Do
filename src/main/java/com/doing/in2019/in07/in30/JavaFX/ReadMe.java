6326dee3c6cd7cb0f2f8ae9f0546ddc09433564a
Clyde <lkwowfs@163.com>
2019-07-30 17:37:56 +0800

    今天

---
+++ src/main/java/com/doing/in2019/in07/in30/JavaFX/ReadMe.java
@@ -0,0 +1,18 @@
+package com.doing.in2019.in07.in30.JavaFX;
+
+public class ReadMe {
+
+	/**
+	 * JavaFX:
+	 * 
+	 * 		想做个桌面软件，实现一些功能，做一个符合自己习惯的日程程序，顺便控制硬件，方便不懂技术的亲属可以随便控制，而不弄坏我家的自动化们。
+	 * 
+	 * 
+	 * 		Q：JavaFX可以构建什么?
+	 * 		A：
+	 * 			1.Ensemble8包括动画、图表、控件，同样可以在ARM平台上的JavaFX中运行???
+	 * 			2.????
+	 * 
+	 * 		
+	 */
+}
